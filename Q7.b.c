b1117dc021d37e4b9d35dbb7bcb02cdfcab57dbe
Salman <salmannazeer477@gmail.com>
2025-12-03 13:39:13 +0530

    added Q7.b

--- Q7.b.c
+++ Q7.b.c
@@ -5,5 +5,26 @@ int main(){
     printf("Enter rows and columns:");
     scanf("%d %d", &row, &col);
     int a[row][col], b[row][col], c[row][col];
+    printf("Enter matrix A:\n");
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            scanf("%d", &a[i][j]);
+    
+    printf("Enter matrix B:\n");
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            scanf("%d", &b[i][j]);
+
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            c[i][j] = a[i][j] + b[i][j];
+
+    printf("Sum matrix:\n");
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++)
+            printf("%d ", c[i][j]);
+        printf("\n");
+    }
+    return 0;
     
 }
